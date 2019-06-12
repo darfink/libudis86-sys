d4a2e4c8fc0b40cd59fd9bfad0f4eba132368136
Elliott Linder <elliott.darfink@gmail.com>
2019-06-13 00:15:23 +0200

    refactor: replace 'gcc-rs' with 'cc-rs'

--- build.rs
+++ build.rs
@@ -1,26 +1,15 @@
-extern crate gcc;
-
-use std::env;
-use std::path::Path;
+extern crate cc;
 
 fn main() {
-    let udis_dir = Path::new("libudis86");
-    let files = [
-        "decode.c",
-        "itab.c",
-        "syn-att.c",
-        "syn-intel.c",
-        "syn.c",
-        "udis86.c"
-    ];
-
-    let mut config = gcc::Config::new();
-    for file in files.iter() {
-        config.file(udis_dir.join(file));
-    }
-
-    config.include(udis_dir)
-          .flag("-includestring.h")
-          .compile("libudis86.a");
-    println!("cargo:root={}", env::var("OUT_DIR").unwrap());
+  cc::Build::new()
+    .files(&[
+      "libudis86/decode.c",
+      "libudis86/itab.c",
+      "libudis86/syn-att.c",
+      "libudis86/syn-intel.c",
+      "libudis86/syn.c",
+      "libudis86/udis86.c"
+    ])
+    .flag("-includestring.h")
+    .compile("udis86");
 }
